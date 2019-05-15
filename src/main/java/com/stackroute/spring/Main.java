3c9b104166c16e82cd59b3f51761543f28d7c8d6
bhavyadesai <bhavya.desai23@gmail.com>
2019-05-15 22:30:57 +0530

    fifth task intial commit

--- src/main/java/com/stackroute/spring/Main.java
+++ src/main/java/com/stackroute/spring/Main.java
@@ -1,14 +1,22 @@
 package com.stackroute.spring;
 
 import com.stackroute.spring.beans.Movie;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.Resource;
 
 public class Main {
     public static void main(String[] args) {
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
-        Movie movie = (Movie)applicationContext.getBean("movie");
-        System.out.println(movie.getActor());
+
+
+        ApplicationContext applicationContext3=new ClassPathXmlApplicationContext("beans.xml");
+
+        Movie movie=(Movie)applicationContext3.getBean("movie4");
+
+        System.out.println(movie.toString());
+
 
     }
 }
--- src/main/java/com/stackroute/spring/beans/Actor.java
+++ src/main/java/com/stackroute/spring/beans/Actor.java
@@ -1,30 +1,70 @@
 package com.stackroute.spring.beans;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.beans.factory.annotation.Autowired;
 
-public class Actor {
+public class Actor implements BeanNameAware, BeanFactoryAware
+{
+
     private String name;
-    private String gender;
     private int age;
+    public Actor(String name, int age, String gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
 
+    public String getGender() {
+        return gender;
+    }
 
     @Override
     public String toString() {
         return "Actor{" +
                 "name='" + name + '\'' +
-                ", gender='" + gender + '\'' +
                 ", age=" + age +
+                ", gender='" + gender + '\'' +
                 '}';
+    }
+
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    private String gender;
+    public Actor(){}
+
+    public Actor(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
-    public void setGender(String gender) {
-        this.gender = gender;
+    public int getAge() {
+        return age;
     }
 
     public void setAge(int age) {
         this.age = age;
     }
 
+    @Override
+    public void setBeanName(String s) {
+        System.out.println("in BeanNameAware "+s);
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("In BeanFactoryAware "+beanFactory );
+    }
 }
